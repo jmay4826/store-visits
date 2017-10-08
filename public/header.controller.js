b2e2e305486a9d73fde0edefc51f18b1f824b11e
Jonathan May <jmay4826@gmail.com>
2017-10-07 20:55:48 -0500

    rough side nav

--- public/header.controller.js
+++ public/header.controller.js
@@ -1,5 +1,10 @@
-angular.module('floorplan').controller('headerController', function ($scope, headerService) {
-  // $timeout(() => ($scope.location = $stateParams.id), 100);
-
-  $scope.title = headerService.getTitle;
-});
+angular
+  .module('floorplan')
+  .controller('headerController', function ($scope, $mdSidenav, headerService, authService) {
+    // $timeout(() => ($scope.location = $stateParams.id), 100);
+    $scope.user = authService.currentUser;
+    $scope.title = headerService.getTitle;
+    $scope.toggleNav = function () {
+      $mdSidenav('nav').toggle();
+    };
+  });
--- public/services/auth.service.js
+++ public/services/auth.service.js
@@ -1,8 +1,13 @@
 angular.module('floorplan').service('authService', function ($http, $q) {
   let currentUser;
+  const that = this;
+  this.currentUser = { username: '', type: '' };
+
   this.login = (username, password) =>
     $http.post('/auth/login', { username, password }).then((response) => {
       currentUser = response.data.passport.user;
+      that.currentUser.type = response.data.passport.user.type;
+      that.currentUser.username = response.data.passport.user.username;
     });
   this.register = (username, password) => $http.post('/auth/register', { username, password });
 
@@ -13,6 +18,11 @@ angular.module('floorplan').service('authService', function ($http, $q) {
         .get('/authcheck')
         .then((response) => {
           currentUser = response.data;
+
+          that.currentUser.type = response.data.type;
+          that.currentUser.username = response.data.username;
+          console.log('that', that.currentUser);
+
           defer.resolve(currentUser);
         })
         .catch(err => defer.resolve());

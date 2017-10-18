1eb63946f731ba5573c6ca95d650618ec9909551
Jonathan May <jmay4826@gmail.com>
2017-10-18 14:31:19 -0500

    floorplan size fix

--- public/locations/location.controller.js
+++ public/locations/location.controller.js
@@ -21,9 +21,14 @@ angular
     $scope.whichComments = { complete: false };
     $scope.highlighted = 1;
 
-    $scope.location.floorplanStyle =
-      $scope.location.height >= $scope.location.width ? { height: '75vh' } : { width: '95vw' };
-    console.log($scope.location);
+    if ($scope.location.height >= $scope.location.width) {
+      $scope.floorplanStyle = { height: '75vh' };
+    } else {
+      $scope.floorplanStyle = { width: '95vw' };
+    }
+    // $scope.floorplanStyle =
+    //   $scope.location.height >= $scope.location.width ? { height: '75vh' } : { width: '95vw' };
+    // console.log($scope.location);
 
     headerService.setTitle(`${$scope.location.name} (${$scope.location.id})`);
     const menuItems = [

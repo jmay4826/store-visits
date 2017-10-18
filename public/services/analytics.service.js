6d497d757123cd98a8b3b8346552aa9ba441bd0b
Jonathan May <jmay4826@gmail.com>
2017-10-18 11:41:57 -0500

    make analytics service http calls consistent, ng-cloak uiview

--- public/services/analytics.service.js
+++ public/services/analytics.service.js
@@ -1,9 +1,9 @@
 angular.module('floorplan').service('analyticsService', function ($http) {
   this.getAnalytics = () => $http.get('/api/analytics');
-  this.getlocationResolutionTime = () => $http.get('api/analytics?q=time&for=locations');
-  this.gettagResolutionTime = () => $http.get('api/analytics?q=time&for=tags');
-  this.gettagBreakdownByLocation = () => $http.get('api/analytics?q=tags');
-  this.gettimeDetailForLocation = locationid => $http.get(`api/analytics/${locationid}/time`);
+  this.getlocationResolutionTime = () => $http.get('/api/analytics?q=time&for=locations');
+  this.gettagResolutionTime = () => $http.get('/api/analytics?q=time&for=tags');
+  this.gettagBreakdownByLocation = () => $http.get('/api/analytics?q=tags');
+  this.gettimeDetailForLocation = locationid => $http.get(`/api/analytics/${locationid}/time`);
   this.getcommentsByTag = dataObject =>
-    $http.get(`api/analytics/tags?category=${dataObject.category}&subcategory=${dataObject.subcategory}&title=${dataObject.title}`);
+    $http.get(`/api/analytics/tags?category=${dataObject.category}&subcategory=${dataObject.subcategory}&title=${dataObject.title}`);
 });
